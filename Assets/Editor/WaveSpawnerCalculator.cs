e4cd8a2abc293f8b61534ae1ddb2c849714751c1
Alex Kong <alexkong07@gmail.com>
2021-09-26 05:15:11 -0500

    Added custom spawn calculator editor script..
    Added custom editor script that creates a built-in calculator for the spawn manager script.

---
+++ Assets/Editor/WaveSpawnerCalculator.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SpawnManager))]
+public class WaveSpawnerCalculator : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        SpawnManager waveSpawner = (SpawnManager)target;
+
+        EditorGUILayout.LabelField("Spawn Tier: " + waveSpawner.UISpawnTier());
+        EditorGUILayout.LabelField("Time Scale: " + waveSpawner.UITimeScale());
+        EditorGUILayout.LabelField("Spawn Budget: " + waveSpawner.UICalculateSpawnBudget());
+    }
+}
