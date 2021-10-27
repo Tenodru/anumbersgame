ae8d1a368a7543ed8e032fa4a7cf684a41301446
Alex Kong <alexkong07@gmail.com>
2021-10-26 20:36:04 -0500

    Implemented Game Over and Restart.
    Game now ends when player health reaches 0. game can be restarted from here.

---
+++ Assets/Scripts/GameStateHandler.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStateHandler : MonoBehaviour
+{
+    [Header("Game Over UI")]
+    public GameObject gameOverUI;
+
+    // References
+    Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+        gameOverUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.playerStats.GetCurrentHealth() <= 0)
+        {
+            GameOver();
+        } 
+    }
+
+    public void GameOver()
+    {
+        PauseGame();
+        player.gameObject.SetActive(false);
+        gameOverUI.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        ResumeGame();
+        gameOverUI.SetActive(false);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+    }
+}
