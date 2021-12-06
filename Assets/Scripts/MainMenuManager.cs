93109fc5705783b5babe45cc977f6bbf548797c1
Alex Kong <alexkong07@gmail.com>
2021-12-05 18:42:10 -0600

    Added Quit functionality.
    Main Menu now has a quit button with confirmation screen.

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -8,10 +8,17 @@ using UnityEngine.SceneManagement;
 /// </summary>
 public class MainMenuManager : MonoBehaviour
 {
+    [Header("MainMenu Canvas References")]
+    public GameObject mainMenuScreen;
+
+    [Header("ExitGame Canvas References")]
+    public GameObject exitGameConfirmScreen;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainMenuScreen.SetActive(true);
+        exitGameConfirmScreen.SetActive(false);
     }
 
     // Update is called once per frame
@@ -27,5 +34,22 @@ public class MainMenuManager : MonoBehaviour
     {
         GameManager.current.StartTime();
         SceneManager.LoadScene("Arena");
+    }
+
+    public void ExitGame()
+    {
+        mainMenuScreen.SetActive(false);
+        exitGameConfirmScreen.SetActive(true);
+    }
+
+    public void ConfirmExit()
+    {
+        Application.Quit();
+    }
+
+    public void GoBack()
+    {
+        mainMenuScreen.SetActive(true);
+        exitGameConfirmScreen.SetActive(false);
     }
 }
